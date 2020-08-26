a5b9d1309455b7f2eb56406f334aecf43110e385
antiorum <antiorum@inbox.ru>
2020-08-26 09:32:05 +0400

    fix view&stored

--- task2/3_view&stored_procedure.sql
+++ task2/3_view&stored_procedure.sql
@@ -4,19 +4,17 @@ GO
 CREATE PROCEDURE GetOrdersByCity
     @city NVARCHAR(200)	
 AS
-SELECT * FROM Orders WHERE CustomerId in (SELECT DISTINCT Id FROM Customers WHERE City = @city)
+SELECT * FROM Orders WHERE SellerId in (SELECT DISTINCT Id FROM Sellers WHERE City = @city)
 
 GO
 CREATE VIEW SellersInfo AS
-SELECT t.Id as Id,
-		t.LastName as SecondName,
-		t.FirstName as FirstName,
-		t.MiddleName as MiddleName,
-		t.CommissionPercentage as CommissionPercentage,
-		t.ComissionSumm as ComissionSumm
+SELECT s.Id as Id,
+		s.LastName as LastName,
+		s.FirstName as FirstName,
+		s.MiddleName as MiddleName,
+		s.CommissionPercentage as CommissionPercentage,
+		(s.CommissionPercentage/100 * o.OrdersSumm) as ComissionSumm
 from
-	(select s.Id, s.LastName, s.FirstName, s.MiddleName, s.CommissionPercentage, (s.CommissionPercentage/100 * o.OrdersSumm) as ComissionSumm
-	from
 	Sellers as s,
 	(select SellerId, SUM(Summ) as OrdersSumm from Orders group by SellerId) as o 
-	where s.Id = o.SellerId) as t
+	where s.Id = o.SellerId
